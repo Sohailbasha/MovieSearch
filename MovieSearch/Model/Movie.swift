02835d8845b8662c165e96b5fc0a043fd0397d85
Sohailbasha <shlbsh94@gmail.com>
2018-01-03 15:40:51 -0500

    added Movie model file

---
+++ MovieSearch/Model/Movie.swift
@@ -0,0 +1,18 @@
+//
+//  Movie.swift
+//  MovieSearch
+//
+//  Created by Ilias Basha on 1/3/18.
+//  Copyright © 2018 Sohail. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Decodable {
+    let title: String?
+    let posterPath: String?
+    let overview: String?
+    let releaseDate: String?
+    let backdropPath: String?
+    let releaseDate: String?
+}
