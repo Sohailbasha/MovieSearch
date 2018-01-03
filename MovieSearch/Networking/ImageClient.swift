be61ebec11be09f01c5be86f8bb15fcf8408c6d3
Sohailbasha <shlbsh94@gmail.com>
2018-01-03 18:30:50 -0500

    Added an ImageClient file. Implement complete later.

---
+++ MovieSearch/Networking/ImageClient.swift
@@ -0,0 +1,13 @@
+//
+//  ImageClient.swift
+//  MovieSearch
+//
+//  Created by Ilias Basha on 1/3/18.
+//  Copyright © 2018 Sohail. All rights reserved.
+//
+
+import Foundation
+
+class ImageClient {
+    
+}
