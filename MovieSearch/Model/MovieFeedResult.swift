b34e23df3fd20e289fb840d2f2bcbaf1c8751666
Sohailbasha <shlbsh94@gmail.com>
2018-01-03 15:42:33 -0500

    Added MovieFeedResult file to model folder

---
+++ MovieSearch/Model/MovieFeedResult.swift
@@ -0,0 +1,13 @@
+//
+//  MovieFeedResult.swift
+//  MovieSearch
+//
+//  Created by Ilias Basha on 1/3/18.
+//  Copyright © 2018 Sohail. All rights reserved.
+//
+
+import Foundation
+
+struct MovieFeedResult: Decodable {
+    let results: [Movie]?
+}
