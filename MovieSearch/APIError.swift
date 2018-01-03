7845dd8a0e1b9ab81296c0c89a6336f92f417fd8
Sohailbasha <shlbsh94@gmail.com>
2018-01-03 15:31:30 -0500

    fixed a typo (jsonparsingFailure -> jsonParsingFailure)

--- MovieSearch/APIError.swift
+++ MovieSearch/APIError.swift
@@ -12,14 +12,14 @@ enum APIError: Error {
     case requestFailed
     case invalidData
     case responseUnsuccessful
-    case jsonparsingFailure
+    case jsonParsingFailure
     case jsonConversionFailure
     var localizedDescription: String {
         switch self {
         case .requestFailed: return "Request Failed"
         case .invalidData: return "Invalid Data"
         case .responseUnsuccessful: return "Response Unsuccessful"
-        case .jsonparsingFailure: return "JSON Parsing Failure"
+        case .jsonParsingFailure: return "JSON Parsing Failure"
         case .jsonConversionFailure: return "JSON Conversion Failure"
         }
     }
