83a885b94929f8f6dc92f254b5970135039b31a3
Sohailbasha <shlbsh94@gmail.com>
2018-01-03 16:51:45 -0500

    Added two new files to handle fetching and showing image urls.

---
+++ MovieSearch/Model/Image.swift
@@ -0,0 +1,13 @@
+//
+//  Image.swift
+//  MovieSearch
+//
+//  Created by Ilias Basha on 1/3/18.
+//  Copyright © 2018 Sohail. All rights reserved.
+//
+
+import Foundation
+
+struct Image {
+    // TODO: Empliment relevant properties
+}
---
+++ MovieSearch/Networking/ImageFeed.swift
@@ -0,0 +1,27 @@
+//
+//  ImageFeed.swift
+//  MovieSearch
+//
+//  Created by Ilias Basha on 1/3/18.
+//  Copyright © 2018 Sohail. All rights reserved.
+//
+
+import Foundation
+
+enum ImageFeed {
+    case poster
+    case backdrop
+}
+
+extension ImageFeed: Endpoint {
+    var base: String {
+        <#code#>
+    }
+    
+    var path: String {
+        <#code#>
+    }
+    
+    
+}
+
